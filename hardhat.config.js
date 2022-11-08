6f4140e7579939a8f70069b0677c5836e3e1acd0
Giorgio Villani <15069760+giorgio-villani@users.noreply.github.com>
2022-11-08 22:54:20 +0000

    changed test filename; added a few more tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,13 +5,13 @@ module.exports = {
   solidity: "0.8.14",
   networks: {
     goerli: {
-      url: `https://mainnet.infura.io/v3/` + process.env.API_KEY,
+      url: `https://goerli.infura.io/v3/` + process.env.API_KEY,
       chainId: 5,
     },
-    hardhat: {
-      forking: {
-        url: `https://mainnet.infura.io/v3/` + process.env.API_KEY,
-      },
-    },
+    // hardhat: {
+    //   forking: {
+    //     url: `https://mainnet.infura.io/v3/` + process.env.API_KEY,
+    //   },
+    // },
   },
 };
